0e5fa4237463e8f036a79d66c77c1e9a79ae3c11
mikeydoesit <msquireobeng@gmail.com>
2023-06-05 09:56:35 +0000

    fixed build failure due to import path

--- src/routes/(app)/about/impact/+page.svelte
+++ src/routes/(app)/about/impact/+page.svelte
@@ -1,6 +1,6 @@
 <script>
     import { fade } from 'svelte/transition';
-    import TwoColumnHero from '../../../lib/components/TwoColumnHero.svelte';
+    import TwoColumnHero from '../../../../lib/components/TwoColumnHero.svelte';
 </script>
 
 <main transition:fade>
